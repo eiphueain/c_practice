7fe1219932b0a67b2a8751d0cf37ee5be0da06bc
Ei Phu Eain <eiphueain@gmail.com>
2026-05-17 01:53:49 +0200

    make main file

---
+++ shoppinglist.c
@@ -0,0 +1,11 @@
+/*
+Created by eiphueain on 17.05.2026.
+Program objective: receive item, price and amount from user
+and calculates the total to print a string out
+*/
+
+#include <stdio.h>
+int main() {
+    // variable declaration
+
+}
